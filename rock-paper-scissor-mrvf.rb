4e1e8193d00203f822e5e68b2903f1d0a36da7bc
Filippo Venturini <filippo.20u@gmail.com>
2020-11-24 14:54:13 -0800

    rock paper scissors done

--- rock-paper-scissor-mrvf.rb
+++ rock-paper-scissor-mrvf.rb
@@ -6,8 +6,6 @@ p 'Player2 type rock, paper or scissors'
 player2 = gets.chomp
 
 
-
-
 # rock, paper, scissors same input => "draw again"
 if player1.to_s == 'rock' && player2.to_s == 'rock' 
      puts "it's a draw"
@@ -34,30 +32,32 @@ elsif player1.to_s == 'rock' && player2.to_s == 'scissors'
 elsif player1.to_s == 'scissors' && player2.to_s == 'rock'
     puts 'Player2 wins'
 
+else
+    puts "I didn't understand you. Please try again."
 end
 
 
 
-comparing computer choice against user choice
-if $user_input == $choice
-    puts "I chose #{$choice} too. It's a tie. Let's try again."
-    game
-  else
-    if $user_input == "rock" && $choice == "paper"
-      puts "I chose paper. Paper wraps rock. I win."
-    elsif $user_input == "rock" && $choice == "scissors"
-      puts "I chose scissors. Rock destroys scissors. You win."
-    elsif $user_input == "paper" && $choice == "rock"
-      puts "I chose rock. Paper wraps rock. You win."
-    elsif $user_input == "paper" && $choice == "scissors"
-      puts "I chose scissors. Scissors cut paper. I win."
-    elsif $user_input == "scissors" && $choice == "rock"
-      puts "I chose rock. Rock destroys scissors. I win."
-    elsif $user_input == "scissors" && $choice == "paper"
-      puts "I chose paper. Scissors cut paper. You win."
-    else
-      puts "I didn't understand you. Please try again."
-      game
-    end #end nested if statement
-  end #end if statement
-end #end game def
+# comparing computer choice against user choice
+# if $user_input == $choice
+#     puts "I chose #{$choice} too. It's a tie. Let's try again."
+#     game
+#   else
+#     if $user_input == "rock" && $choice == "paper"
+#       puts "I chose paper. Paper wraps rock. I win."
+#     elsif $user_input == "rock" && $choice == "scissors"
+#       puts "I chose scissors. Rock destroys scissors. You win."
+#     elsif $user_input == "paper" && $choice == "rock"
+#       puts "I chose rock. Paper wraps rock. You win."
+#     elsif $user_input == "paper" && $choice == "scissors"
+#       puts "I chose scissors. Scissors cut paper. I win."
+#     elsif $user_input == "scissors" && $choice == "rock"
+#       puts "I chose rock. Rock destroys scissors. I win."
+#     elsif $user_input == "scissors" && $choice == "paper"
+#       puts "I chose paper. Scissors cut paper. You win."
+#     else
+#       puts "I didn't understand you. Please try again."
+#       game
+#     end #end nested if statement
+#   end #end if statement
+# end #end game def
