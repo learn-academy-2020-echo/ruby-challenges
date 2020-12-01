028d07ba1607305a68f14b28666701a57ca04f42
AlanHausinger <alanh621@gmail.com>
2020-12-01 14:43:35 -0800

    Inheritancechanges

--- inheritance-alan-filippo.rb
+++ inheritance-alan-filippo.rb
@@ -17,17 +17,32 @@ class Animal
 
   end
 
+class Fish < Animal
+  attr_accessor :cold_blooded
+  def initialize description
+    super description
+    @cold_blooded = "yes"
+  end
+end
+
+class Salmon < Fish
+  attr_accessor :species
+  def initialize (description)
+    super description
+    @species = "atlantic"
+    end
+
+end
 
 
-
-
-
+new_animal = Salmon.new "warmblooded"
+p new_animal
 # anim1 = Animal.new  "test"
-anim2 = Animal.new ("fish")
-# p anim2.aging
-# p anim2
-anim2.aging
-anim2.aging
-anim2.aging
-p anim2.animal_info
+# anim2 = Animal.new ("fish")
+# # p anim2.aging
+# # p anim2
+# anim2.aging
+# anim2.aging
+# anim2.aging
+# p anim2.animal_info
 # p animal_info
