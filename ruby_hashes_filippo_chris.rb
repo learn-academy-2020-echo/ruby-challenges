06fc2eededd1322468da1ae9e3d0ba8eebd1552b
chrisolorobin <camorton88@gmail.com>
2020-11-30 16:03:07 -0800

    hash challenges

--- ruby_hashes_filippo_chris.rb
+++ ruby_hashes_filippo_chris.rb
@@ -8,39 +8,42 @@ my_phone [:photos] = "gallery of photos taken with phone"
 my_phone [:camera] = "app used to take photos"
 my_phone [:gmail] = "google email application"
 
-#return value from my_phone
-# p my_phone [:gmail]
-#
-#
-# #updating values
-# my_phone [:photos] = "snapchat"
-# p my_phone [:photos]
-# my_phone [:gmail] = "email"
-#
-# #updating key
-# my_phone [:facebook] = my_phone.delete :camera
-# p my_phone
-#
-#
-# #delete key: value pairs
-# my_phone.delete :facebook
-# my_phone.delete :gmail
-#
-# #create method
-# def my_phone_info hash
-#   hash.map do |key, value|
-#   "my #{key} is #{value}"
-#   end
-# end
-# p my_phone_info my_phone
+return value from my_phone
+p my_phone [:gmail]
+
+
+#updating values
+my_phone [:photos] = "snapchat"
+p my_phone [:photos]
+my_phone [:gmail] = "email"
+
+#updating key
+my_phone [:facebook] = my_phone.delete :camera
+p my_phone
+
+
+#delete key: value pairs
+my_phone.delete :facebook
+my_phone.delete :gmail
+
+#create method
+def my_phone_info hash
+  hash.map do |key, value|
+  "my #{key} is #{value}"
+  end
+end
+p my_phone_info my_phone
 
 
 #stretch challenge- array with app name capitalized and info
-def my_phone_all hash
-  hash.map do |key, value|
-  key.upcase 
-end
+# def my_phone_all hash
+#   hash.map do |key, value|
+#   key.upcase
+# end
+#
+# end
+#
+# p my_phone_all my_phone
 
-end
 
-p my_phone_all my_phone
+#create method
