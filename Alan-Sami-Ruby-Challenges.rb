f1d5bc9ef921170d4f95d2172de981a7adf31a96
mabrouk10 <samimabrouk10@gmail.com>
2020-11-30 16:38:22 -0800

    challenges done

--- Alan-Sami-Ruby-Challenges.rb
+++ Alan-Sami-Ruby-Challenges.rb
@@ -38,4 +38,64 @@ def my_phone (phone)
   end
 end
 
-p my_phone phone
+# p my_phone phone
+
+
+class Task 
+  def initialize title, des, done
+  @title = title
+  @des = des
+  @done = done
+  @status = "status done"
+  end
+
+  def title
+    @title
+  end
+
+  def des
+    @des
+  end
+
+  def done
+    @done
+  end
+
+  def status
+    @status
+  end
+
+end
+
+# task1 = Task.new 'chores', 'vaccum', 'finish'
+# p task1.title
+# p task1.des
+# p task1.done
+# p task1.status
+
+
+class Color
+  attr_accessor :color1, :color2, :color3
+  def initialize(color1, color2, color3)
+    @color1 = color1
+    @color2 = color2
+    @color3 = color3
+    # p "These are our colors: #{color1} #{color2} and #{color3}"
+  end
+
+    def colors
+      p "These are our colors: #{@color1} #{@color2} and #{@color3}"
+  end
+end
+
+newColor = Color.new('blue', 'red', 'green')
+# p newColor.color1
+# p newColor.color2
+# p newColor.color3
+
+newColor.color1 = "yellow"
+
+newColor.colors
+
+
+
