06a29e39f2b5445126840d8c4588e52b25c74da5
chrisolorobin <camorton88@gmail.com>
2020-12-01 11:21:49 -0800

    salmon class added

--- inheritance.rb
+++ inheritance.rb
@@ -3,6 +3,9 @@
 # Story: As a developer, I can give my Animal an age of 0 upon creation.
 # Story: As a developer, I can age my Animal up one year at a time.
 # Story: As a developer, I can return my Animal's age, as well as if they're alive. Hint: Use attr_accessor as well as an initialize method.
+# Story: As a developer, I can create a Fish that inherits from Animal.
+# Story: As a developer, I can initialize my Salmon to be a specific species (Atlantic, Sockeye, etc).
+# Story: As a developer, I can see that my Salmon is cold-blooded.
 
 
 class Animal
@@ -32,25 +35,73 @@ end
 
 test = Animal.new
 p test.increase_age
-p test.change_status
+p test.increase_age
+p test.increase_age
+p test.increase_age
+#p test.change_status
 p test.get_animal_info
-
 
 # Story: As a developer, I can create a Fish that inherits from Animal.
 
-# Story: As a developer, I can initialize all of my fish to be cold_blooded (yes, there is one fish who is technically fully warm-blooded but we aren't going to talk about that).
+class Fish < Animal
+  attr_accessor :cold_blooded
+  def initialize
+    super()
+    @cold_blooded = true
+  end
+end
 
-# Story: As a developer, I can create a Salmon that inherits from Fish.
+test2 = Fish.new
+p test2
+test2.increase_age
+p test2.get_animal_info
 
 # Story: As a developer, I can initialize my Salmon to be a specific species (Atlantic, Sockeye, etc).
-
 # Story: As a developer, I can see that my Salmon is cold-blooded.
-
 # Story: As a developer, I can age my Salmon up.
-
 # Story: As a developer, I can see a message that tells me all of my Salmon's information.
-
 # Story: As a developer, if my Salmon reaches the ripe old age of 4, I can make it die peacefully after a full and happy life. Hint: You will need a method that changes the status of alive in the initialize method of Animal.
+
+class Salmon < Fish
+  attr_accessor :species
+  def initialize (species)
+    super()
+    @species = species
+  end
+  
+  def get_salmon_info 
+   if @age == 4
+      change_status
+      "This salmon is not alive, but lived to the ripe old age of #{age} years old"
+   else
+      "This salmon is alive and is #{age} years old"
+   end
+  end
+
+  # def get_animal_info 
+  #   if @status == true 
+  #      "This animal is alive and is #{age} years old"
+  #   else 
+  #       "This animal is not alive, but it was #{age} years old when it died"
+  #   end
+  # end
+
+
+end
+
+salmon1 = Salmon.new "atlantic"
+#p salmon1.cold_blooded
+ salmon1.increase_age
+ salmon1.increase_age 
+#  salmon1.increase_age
+#  salmon1.increase_age
+p salmon1.get_salmon_info
+
+
+
+
+
+
 
 # Story: As a developer, I can create a Mammal that inherits from Animal.
 
