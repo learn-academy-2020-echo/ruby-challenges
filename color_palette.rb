784a1dacab1ffc2727f271f5e2ac07e745625e05
AKFosterDev <akfoster2015@gmail.com>
2020-11-30 16:51:06 -0800

    task list complete

---
+++ color_palette.rb
@@ -0,0 +1,10 @@
+
+
+# Challenge: Color Palette
+# Use attr_accessor for this challenge.
+
+# Story: As a developer, I can create a ColorPalette.
+# Story: As a developer, I can initialize my color palette to contain 3 colors.
+# Story: As a developer, I can print the values of each individual color.
+# Story: As a developer, I can print a sentence that tells me all of the colors in my palette.
+# Story: As a developer, I can change one or more of the colors in my palette.
